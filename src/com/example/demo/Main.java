77515b39b02a0f8b6297db6f7e8098450856e381
WakingInsect <qq1350574536@163.com>
2021-05-22 08:59:03 +0800

    new project

---
+++ src/com/example/demo/Main.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * 程序的入口
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome use this program!");
+    }
+}
