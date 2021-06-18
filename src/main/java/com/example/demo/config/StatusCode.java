5b0314c065db1bf1cc5e14896eb36d784677f42d
WakingInsect <qq1350574536@163.com>
2021-06-18 10:58:37 +0800

    删除一些不必要的东西

--- src/main/java/com/example/demo/config/StatusCode.java
+++ src/main/java/com/example/demo/config/StatusCode.java
@@ -1,25 +1,10 @@
 package com.example.demo.config;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * 响应行中使用的状态码及其描述的常量类
  */
-public final class StatusCode {
-    private final static int OK = 200;
-    private final static int MOVED_PERMANENTLY = 301;
-    private final static int FOUND = 302;
-    private final static int NOT_FOUND = 404;
-    private final static int INTERNAL_SERVER_ERROR = 500;
-
-    private final static Map<Integer,String> desc = new HashMap<Integer,String>(){
-        {
-            put(200, "OK");
-            put(301, "MOVED PERMANENTLY");
-            put(302, "FOUND");
-            put(404, "NOT FOUND");
-            put(500,"INTERNAL SERVER ERROR");
-        }
-    };
+public enum StatusCode {
+    OK, MOVED_PERMANENTLY, FOUND, NOT_FOUND, INTERNAL_SERVER_ERROR;
 }
--- src/main/java/com/example/demo/utils/PathUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-/**
- * 获取文件路径的工具
- */
-public class PathUtil {
-    public static <T> String getPath(Class<T> para) {
-        return para.getResource("").getPath();
-    }
-}
